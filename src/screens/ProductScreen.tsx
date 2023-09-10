f3b640857e7d8512ae96a8581b66a6b9dcbe3bc1
Albert Urbina <johan50693@gmail.com>
2023-09-10 17:56:02 -0400

    Diseño de pantalla del producto

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {useEffect} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TextInput, Button } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProductsStackParams } from '../navigator/ProductsNavigator';
 
@@ -19,9 +19,62 @@ export const ProductScreen = ({route,navigation}: Props) => {
 
 
   return (
-      <View>
-        <Text> {id} </Text>
-        <Text> {name} </Text>
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.label}>Nombre del producto:</Text>
+          <TextInput
+            placeholder="Producto"
+            style= {styles.textInput}
+
+          />
+
+          {/* Selector / Pcker */}
+          <Text style={styles.label}>Categoria:</Text>
+
+          <Button
+            title="Guardar"
+            onPress={ () => {}}
+            color="#5856d6"
+          />
+
+          <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 10}} >
+            <Button
+              title="Cámara"
+              onPress={ () => {}}
+              color="#5856d6"
+            />
+
+            <View style={{width: 10}} />
+
+            <Button
+              title="Galeria"
+              onPress={ () => {}}
+              color="#5856d6"
+            />
+          </View>
+
+        </ScrollView>
       </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex:1,
+      marginTop: 10,
+      marginHorizontal: 20,
+    },
+    label: {
+      fontSize: 18,
+    },
+    textInput: {
+      borderWidth: 1,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      borderRadius: 20,
+      borderColor: 'rgba(0,0,0,0.2)',
+      height: 45,
+      marginTop: 5,
+      marginBottom: 15,
+    },
+});
